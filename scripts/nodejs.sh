7407dac8a9f56eb77fc39ebc74ffadfec74dfee1
Corey Connor <coreydconnor@gmail.com>
2021-11-11 15:51:42 +0000

    configure yarn installation to go along wtih nodehs

--- scripts/nodejs.sh
+++ scripts/nodejs.sh
@@ -3,3 +3,7 @@
 # Set the node target and install
 sudo dnf module enable nodejs:$1 -y
 sudo dnf install nodejs -y
+
+# Install yarn
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+dnf install yarn -y
